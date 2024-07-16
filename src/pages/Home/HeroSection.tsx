b42a5bb43eb7823f8f4e14830b1072615b5d5afd
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-16 17:12:04 +0600

    Hero section modified

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -1,10 +1,49 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const HeroSection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      plugins={[Autoplay({ delay: 2000 })]}
+      className="w-12/12 mx-auto"
+    >
+      <CarouselContent>
+        <CarouselItem className="basis-full lg:basis-1/2">
+          <div className="p-0">
+            <img
+              src="https://www.urvann.com/s/6176774ef575bbd2b3331c8a/64443ecd0c5dcaf96dbd4f23/flowering-plants_new-864x432.png"
+              alt=""
+            />
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basis-full lg:basis-1/2">
+          <div className="p-0">
+            <img
+              src="https://www.urvann.com/s/6176774ef575bbd2b3331c8a/64443f4178224d7aae14c0f5/indoor-plants_new-864x432.png"
+              alt=""
+            />
+          </div>
+        </CarouselItem>
+        <CarouselItem className="basis-full lg:basis-1/2">
+          <div className="p-0">
+            <img
+              src="https://www.urvann.com/s/6176774ef575bbd2b3331c8a/64443f4178224d7aae14c0f5/indoor-plants_new-864x432.png"
+              alt=""
+            />
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      {/* <CarouselPrevious />
+      <CarouselNext /> */}
+    </Carousel>
+  );
 };
-
 export default HeroSection;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "./HeroSection";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
+      <HeroSection/>
     </div>
   );
 };
