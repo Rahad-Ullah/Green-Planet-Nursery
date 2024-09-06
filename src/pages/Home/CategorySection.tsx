5de63509f2156f39c13875287609984d8b91bb8b
Rahad-Ullah <myemail.abc20@gmail.com>
2024-09-06 23:59:33 +0600

    Added Gallery

--- src/pages/Home/CategorySection.tsx
+++ src/pages/Home/CategorySection.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Container from "@/components/shared/Container";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useGetCategoriesQuery } from "@/redux/features/category/categoryApi";
 import { TCategory } from "@/types/TCategory";
 import { Link } from "react-router-dom";
@@ -8,24 +10,35 @@ const CategorySection = () => {
 
   return (
     <Container>
-      <div className="flex flex-wrap justify-center items-center gap-8 md:gap-16 mt-12">
-        {data?.data.map((item: TCategory) => (
-          <Link
-            to={`/categories/${item._id}`}
-            key={item._id}
-            className="flex flex-col items-center gap-2 group cursor-pointer"
-          >
-            <img
-              src={item.image}
-              alt="category image"
-              className="size-24 object-cover rounded-full border-2 border-primary p-1"
-            />
-            <h3 className="group-hover:text-primary font-medium">
-              {item.category}
-            </h3>
-          </Link>
-        ))}
-      </div>
+      {data ? (
+        <div className="flex flex-wrap justify-center items-center gap-8 md:gap-16 mt-12">
+          {data?.data.map((item: TCategory) => (
+            <Link
+              to={`/categories/${item._id}`}
+              key={item._id}
+              className="flex flex-col items-center gap-2 group cursor-pointer"
+            >
+              <img
+                src={item.image}
+                alt="category image"
+                className="size-24 object-cover rounded-full border-2 border-primary p-1"
+              />
+              <h3 className="group-hover:text-primary font-medium">
+                {item.category}
+              </h3>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-wrap justify-center items-center gap-8 md:gap-16 mt-12">
+          {Array.from({ length: 6 }).map((_: any, index: number) => (
+            <div key={index} className="space-y-4 flex flex-col items-center">
+              <Skeleton className="size-24 rounded-full" />
+              <Skeleton className="h-4 w-full" />
+            </div>
+          ))}
+        </div>
+      )}
     </Container>
   );
 };
--- src/pages/Home/ImageGallery.tsx
+++ src/pages/Home/ImageGallery.tsx
@@ -1,10 +1,64 @@
+import Container from "@/components/shared/Container";
+import { useGetProductsQuery } from "@/redux/features/productsApi/productsApi";
+import { TProduct } from "@/types/TProduct";
+import { TProductsQuery } from "@/types/TProductsQuery";
 
 const ImageGallery = () => {
-    return (
-        <div>
-            
+  const query: TProductsQuery = {
+    search: "",
+    category: "",
+    minPrice: 0,
+    maxPrice: 99999,
+    sortBy: "",
+    sortOrder: "",
+    page: 1,
+    limit: 25,
+  };
+
+  const { data } = useGetProductsQuery(query);
+
+  const arr1 = data?.data?.products.slice(0, 4);
+  const arr2 = data?.data?.products.slice(5, 9);
+  const arr3 = data?.data?.products.slice(10, 14);
+  const arr4 = data?.data?.products.slice(15, 19);
+  const arr5 = data?.data?.products.slice(20, 25);
+
+  return (
+    <Container>
+      <div className="pb-16">
+        <h1 className="text-3xl md:text-4xl font-bold text-center py-6 mt-10 pb-10">
+          Gallery
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-4">
+          <div className="space-y-4">
+            {arr1?.map((item: TProduct) => (
+              <img src={item.image} key={item._id} />
+            ))}
+          </div>
+          <div className="space-y-4">
+            {arr2?.map((item: TProduct) => (
+              <img src={item.image} key={item._id} />
+            ))}
+          </div>
+          <div className="space-y-4">
+            {arr3?.map((item: TProduct) => (
+              <img src={item.image} key={item._id} />
+            ))}
+          </div>
+          <div className="space-y-4">
+            {arr4?.map((item: TProduct) => (
+              <img src={item.image} key={item._id} />
+            ))}
+          </div>
+          <div className="space-y-4">
+            {arr5?.map((item: TProduct) => (
+              <img src={item.image} key={item._id} />
+            ))}
+          </div>
         </div>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default ImageGallery;
